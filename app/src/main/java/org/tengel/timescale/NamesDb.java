c75cbf1fbca764dce0632cad553f9f57bff91570
Timo <timo-e@freenet.de>
2021-04-17 11:31:55 +0200

    New Translation: Spanish.
    Merged pull request #5 from ferne with minor changes.

--- app/src/main/java/org/tengel/timescale/NamesDb.java
+++ app/src/main/java/org/tengel/timescale/NamesDb.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 
 public class NamesDb
 {
-    // 0:id, 1:en, 2:de, 3:zh 4:nb
+    // 0:id, 1:en, 2:de, 3:zh 4:nb 5:es
     private HashMap<String, String[]> m_names = new HashMap<String, String[]>();
 
     public NamesDb(InputStream namesStream) throws IOException
@@ -22,7 +22,7 @@ public class NamesDb
                 break;
             }
             String[] lItems = line.split("\t");
-            if (lItems.length != 5)
+            if (lItems.length != 6)
             {
                 throw new IOException("parse failed: " + line);
             }
